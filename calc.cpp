0b78cc0f673b3a520d469493d605a18f3f1db960
Jasson M <Jassonmarquez20@gmail.com>
2026-08-27 16:54:45 -0700

    Testing Commit Changes

---
+++ calc.cpp
@@ -0,0 +1,12 @@
+#include <iostream> 
+using namespace std;
+int main(){
+    int int_x = 1; 
+    int int_y = 2;
+    double double_z = 2.0; 
+    
+    cout<<"int divivded by int : " << int_x / int_y << endl; 
+    cout<<"int divivded by double : " << int_x / double_z << endl; 
+
+
+}
---
+++ isGreaterThan.cpp
@@ -0,0 +1,26 @@
+#include <iostream> 
+using namespace std; 
+
+/*
+* @brief isGreaterThan : returns whether x is greater than y 
+*
+* @return int 
+*/ 
+bool isGreaterThan(double x, int y) {
+    if (x>y){
+        return true; 
+    } else {
+        return false;
+    }
+}
+
+int main (){
+    double x = 33; 
+    int y = 7; 
+
+    if (isGreaterThan(x,y))
+        printf("%f > %i\n", x , y ); 
+    else 
+        printf("%f <= %i\n", x , y );
+    
+}
